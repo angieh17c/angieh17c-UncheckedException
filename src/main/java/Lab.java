739fc1fc0ceab8719a93a5a6cc3d30e2c5352a99
angieh17c <angieh17c@gmail.com>
2023-03-01 06:29:06 +0000

    Completed Unchecked Exceptions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -13,7 +13,12 @@ public class Lab {
      * Notice that errors are separate from exceptions. Errors are external to Java and can occur when some external
      * event happens, like running out of computer memory.
      */
-    public void throwUncheckedException(){
-
+    public void throwUncheckedException() {
+            String[] fruits = { "apple", "banana", "grape", "orange" };
+        
+            for (int i = 0; i <= fruits.length; i++) {
+              System.out.println(fruits[i]);
+            }
+          
     }
 }
